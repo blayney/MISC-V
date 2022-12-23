e4368024a531976044dcab1addd2c3a0ab75e27e
Will Blayney <woblayney@gmail.com>
2022-12-23 18:43:10 +0000

    made CPU rv32i compliant

--- cpu.sv
+++ cpu.sv
@@ -1,38 +1,54 @@
-module cpu(
-  input wire clock,
-  input wire reset,
-  input wire [7:0] instruction,
-  output wire [7:0] result,
-  output wire zero,
-  output wire carry,
-  output wire overflow
+module CPU(
+  input clk,
+  input reset,
+  input [31:0] instruction_memory[],
+  output [31:0] regfile[32],
+  output [31:0] pc
 );
-  reg [15:0] pc; // Program counter
-  reg [7:0] a; // Register A
-  reg [7:0] b; // Register B
-  wire [1:0] alu_op;
-  wire [3:0] rd_addr;
-  wire [3:0] wr_addr;
-  wire wr_enable;
 
-  // Instantiate the register file, instruction memory, ALU, and control unit
-  register_file regfile(.clock(clock), .rd_addr(rd_addr), .wr_addr(wr_addr), .wr_data(result), .wr_enable(wr_enable), .rd_data(b));
-  instruction_memory imem(.address(pc), .instruction(instruction));
-  alu alu(.a(a), .b(b), .opcode(alu_op), .result(result), .zero(zero), .carry(carry), .overflow(overflow));
-  control_unit cu(.instruction(instruction), .alu_op(alu_op), .rd_addr(rd_addr), .wr_addr(wr_addr), .wr_enable(wr_enable));
+// internal signals
+reg [31:0] pc;
+reg [31:0] regfile[32];
+wire [31:0] alu_result;
+wire [4:0] next_pc;
 
-  // Fetch-decode-execute cycle
-  always_ff @(posedge clock or posedge reset) begin
-    if (reset) begin
-      // Initialize PC to 0 on reset
-      pc <= 16'h0000;
-    end else begin
-      // Fetch instruction
-      pc <= pc + 1;
-      // Decode instruction
-      a <= regfile.rd_data;
-      // Execute instruction
-      regfile.wr_data <= result;
-    end
+// instantiate modules
+ALU alu(.opcode(opcode), .a(a), .b(b), .result(alu_result), .next_pc(next_pc));
+InstructionMemory imem(.clk(clk), .reset(reset), .pc(pc), .instruction(instruction));
+ControlUnit cu(.opcode(opcode), .func3(func3), .func7(func7), .alu_op(alu_op), .branch_op(branch_op), .jump_op(jump_op));
+RegisterFile rf(.read_reg_1(read_reg_1), .read_reg_2(read_reg_2), .write_reg(write_reg), .write_data(write_data), .regfile_out_1(regfile_out_1), .regfile_out_2(regfile_out_2), .regfile(regfile));
+
+// main execution loop
+always_ff @(posedge clk) begin
+  if (reset) begin
+    pc <= 0;
+  end else begin
+    // fetch instruction
+    instruction <= instruction_memory[pc];
+    opcode <= instruction[6:0];
+    func3 <= instruction[14:12];
+    func7 <= instruction[31:25];
+    
+    // decode instruction
+    cu.decode();
+    
+    // read operands from register file
+    rf.read();
+    
+    // execute instruction
+    alu.execute();
+    
+    // write result to register file
+    rf.write();
+    
+    // update program counter
+    case (next_pc)
+      PC_PLUS_4: pc <= pc + 4;
+      PC_BRANCH: pc <= alu_result;
+      PC_JALR: pc <= (regfile_out_1 + alu_result) & ~1;
+      PC_JAL: pc <= alu_result;
+    endcase
   end
+end
+
 endmodule
